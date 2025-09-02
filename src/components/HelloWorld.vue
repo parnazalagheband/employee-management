09f368ef7fa98328f9f5f1978c93dc1b5f767005
Parnaz Alagheband <parnazalagheband@gmail.com>
2025-09-02 14:23:59 +0430

    chore: initial commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,5 @@
+<template>
+  <v-container>
+    Hello World!
+  </v-container>
+</template>
---
+++ src/plugins/index.js
@@ -0,0 +1,10 @@
+import router from '@/router'
+import pinia from '@/stores'
+import vuetify from './vuetify'
+
+export function registerPlugins (app) {
+  app
+    .use(vuetify)
+    .use(router)
+    .use(pinia)
+}
